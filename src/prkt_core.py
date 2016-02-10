f8ffc670e52ba5e62a199637ddf538eb8c36803c
Buck Baskin <mobile.wbaskin@gmail.com>
2016-02-10 15:45:08 -0500

    Working/running parakeet core

--- src/prkt_core.py
+++ src/prkt_core.py
@@ -1,8 +1,17 @@
+#!/usr/bin/env python
+
 import rospy
+from viz_feature_sim.msg import Observation
 
 class CamSlam360(object):
     def __init__(self):
+        rospy.init_node('CAMSLAM360')
+        self.cam_sub = rospy.Subscriber('/360cam/features', Observation, self.process_obs)
+        rospy.spin()
+
+    def process_obs(self, observation):
         pass
 
 if __name__ == '__main__':
-    pass
+    CamSlam360()
+
