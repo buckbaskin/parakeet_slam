4f2bb4ce970e680be603cd6be61630f193fb94e2
Buck Baskin <mobile.wbaskin@gmail.com>
2016-03-21 14:18:03 -0400

    Correct the ros node

--- src/prkt_ros.py
+++ src/prkt_ros.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 
+'''
+Create a ROS node that uses the parakeet core to do SLAM
+'''
+
 import rospy
 
-from nav_msgs.msg import Odometry
-from utils import heading_to_quaternion, quaternion_to_heading
-from viz_feature_sim.msg import Observation
-
-import numpy.random.normal as normal # normal(mu, sigma, count)
+from geometry_msgs.msg import Twist
+# from nav_msgs.msg import Odometry
+from prkt_core_v2 import FastSLAM
+from viz_feature_sim.msg import VizScan
 
 class CamSlam360(object):
     '''
@@ -20,13 +23,33 @@ class CamSlam360(object):
         rospy.init_node('CAMSLAM360')
         # prepare filter
 
+        self.core = None
         self.initialize_particle_filter()
-        self.initialize_known_landmarks()
 
         # begin ros updating
-        self.cam_sub = rospy.Subscriber('/360cam/features', Observation, 
+        self.cam_sub = rospy.Subscriber('/360cam/features', VizScan,
             self.measurement_update)
+        self.twist_sub = rospy.Subscriber('/cmd_vel', Twist, self.motion_update)
         rospy.spin()
+
+    def initialize_particle_filter(self):
+        '''
+        Create an instance of FastSLAM algorithm
+        '''
+        self.core = FastSLAM()
+
+    def measurement_update(self, msg):
+        '''
+        Pass along a VizScan message
+        '''
+        self.core.cam_cb(msg)
+
+    def motion_update(self, msg):
+        '''
+        Pass along a Twist message
+        '''
+        self.core.motion_update(msg)
+
 
 if __name__ == '__main__':
     CamSlam360()
