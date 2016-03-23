8b72c8cb2d8c7b9201d2156b45620d66520010f2
Buck Baskin <mobile.wbaskin@gmail.com>
2016-03-23 19:09:00 -0400

    Working on getting testing running on new machine

--- src/import_testing.py
+++ src/import_testing.py
@@ -2,14 +2,24 @@
 
 print('\n...\n')
 
-import sys; print(sys.path)
-print('/home/buck/miniconda2/envs/prkt_env/lib/python2.7/site-packages/' in sys.path)
+# import sys; print(sys.path)
+# print('/home/buck/miniconda2/envs/prkt_env/lib/python2.7/site-packages/' in sys.path)
+
+import scipy
+from scipy.stats import multivariate_normal
 
 print('\n...\n')
 
+import sys
+sys.path.append('/home/buck/ros_ws/src')
 
-import scipy
+print('\n...\n')
 
-print('... Imported scipy')
+from viz_feature_sim.msg import VizScan, Blob
 
-from scipy.stats import multivariate_normal
+print('\n...\n')
+
+import crispy_parakeet
+from crispy_parakeet.src import prkt_core_v2
+
+print('\n...\n')
