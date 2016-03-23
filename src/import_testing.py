fa38a223ef054c38bbee64496e5355bc465b92a8
Buck Baskin <mobile.wbaskin@gmail.com>
2016-03-23 17:41:38 -0400

    Closing out stdr

---
+++ src/import_testing.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+print('\n...\n')
+
+import sys; print(sys.path)
+print('/home/buck/miniconda2/envs/prkt_env/lib/python2.7/site-packages/' in sys.path)
+
+print('\n...\n')
+
+
+import scipy
+
+print('... Imported scipy')
